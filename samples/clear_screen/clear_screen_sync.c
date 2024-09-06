ee7b9f228de4a64a78a53e97ba8d698090610657
Jukka Jylänki <jujjyl@gmail.com>
2024-09-06 11:02:10 +0300

    Add new function wgpu_present_all_rendering_and_wait_for_next_animation_frame() that uses JSPI to break up an infinite rendering loop. Refactor clear_screen_sync.c to use this example.

--- samples/clear_screen/clear_screen_sync.c
+++ samples/clear_screen/clear_screen_sync.c
@@ -3,10 +3,6 @@
 #include <emscripten/em_math.h>
 #include "lib_webgpu.h"
 
-WGpuDevice device;
-WGpuQueue queue;
-WGpuCanvasContext canvasContext;
-
 double hue2color(double hue)
 {
   hue = emscripten_math_fmod(hue, 1.0);
@@ -16,42 +12,49 @@ double hue2color(double hue)
   return 0;
 }
 
-WGPU_BOOL raf(double time, void *userData)
-{
-  WGpuCommandEncoder encoder = wgpu_device_create_command_encoder_simple(device);
-
-  WGpuRenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
-  colorAttachment.view = wgpu_canvas_context_get_current_texture_view(canvasContext);
-
-  double hue = time * 0.00005;
-  colorAttachment.clearValue.r = hue2color(hue + 1.0 / 3.0);
-  colorAttachment.clearValue.g = hue2color(hue);
-  colorAttachment.clearValue.b = hue2color(hue - 1.0 / 3.0);
-  colorAttachment.clearValue.a = 1.0;
-  colorAttachment.loadOp = WGPU_LOAD_OP_CLEAR;
-
-  WGpuRenderPassDescriptor passDesc = {};
-  passDesc.numColorAttachments = 1;
-  passDesc.colorAttachments = &colorAttachment;
-
-  wgpu_render_pass_encoder_end(wgpu_command_encoder_begin_render_pass_1color_0depth(encoder, &passDesc));
-  wgpu_queue_submit_one_and_destroy(queue, wgpu_command_encoder_finish(encoder));
-
-  return EM_TRUE;
-}
-
 int main(int argc, char **argv)
 {
   WGpuAdapter adapter = navigator_gpu_request_adapter_sync_simple();
-  device = wgpu_adapter_request_device_sync_simple(adapter);
-  queue = wgpu_device_get_queue(device);
+  WGpuDevice device = wgpu_adapter_request_device_sync_simple(adapter);
+  WGpuQueue queue = wgpu_device_get_queue(device);
 
-  canvasContext = wgpu_canvas_get_webgpu_context("canvas");
+  WGpuCanvasContext canvasContext = wgpu_canvas_get_webgpu_context("canvas");
 
   WGpuCanvasConfiguration config = WGPU_CANVAS_CONFIGURATION_DEFAULT_INITIALIZER;
   config.device = device;
   config.format = navigator_gpu_get_preferred_canvas_format();
   wgpu_canvas_context_configure(canvasContext, &config);
 
-  wgpu_request_animation_frame_loop(raf, 0);
+  for(;;)
+  {
+    WGpuCommandEncoder encoder = wgpu_device_create_command_encoder_simple(device);
+
+    WGpuRenderPassColorAttachment colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
+    colorAttachment.view = wgpu_canvas_context_get_current_texture_view(canvasContext);
+
+    double hue = emscripten_performance_now() * 0.00005;
+    colorAttachment.clearValue.r = hue2color(hue + 1.0 / 3.0);
+    colorAttachment.clearValue.g = hue2color(hue);
+    colorAttachment.clearValue.b = hue2color(hue - 1.0 / 3.0);
+    colorAttachment.clearValue.a = 1.0;
+    colorAttachment.loadOp = WGPU_LOAD_OP_CLEAR;
+
+    WGpuRenderPassDescriptor passDesc = {};
+    passDesc.numColorAttachments = 1;
+    passDesc.colorAttachments = &colorAttachment;
+
+    wgpu_render_pass_encoder_end(wgpu_command_encoder_begin_render_pass_1color_0depth(encoder, &passDesc));
+    wgpu_queue_submit_one_and_destroy(queue, wgpu_command_encoder_finish(encoder));
+
+    // This sample shows how to use a synchronous-looking infinite rendering loop,
+    // where the following function yields back to the browser event loop using
+    // JSPI. This allows porting applications that use infinite for() loops.
+    // Using this function requires linking with -sJSPI.
+    // Alternatively, one can use more traditional wgpu_request_animation_frame_loop()
+    // callbacks.
+    // Note: there is a possibility that wgpu_request_animation_frame_loop() offers
+    // better performance and input latency compared to using this method, so
+    // prefer using the callback variant whenever possible.
+    wgpu_present_all_rendering_and_wait_for_next_animation_frame();
+  }
 }
