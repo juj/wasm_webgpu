e5b4742fb37fd8759f036946e0195f0ea266dfbe
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 01:15:53 +0300

    Add test for navigator_gpu_request_adapter_sync_simple().

---
+++ test/navigator_gpu_request_adapter_sync_simple.cpp
@@ -0,0 +1,27 @@
+// flags: -sJSPI
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+int ok = 1;
+
+void in_between(void *arg)
+{
+  if (!wgpu_sync_operations_pending()) ok = 0;
+  assert(wgpu_sync_operations_pending() == 1);
+  EM_ASM({ console.log(`In between, ${$0} sync operations pending`); }, wgpu_sync_operations_pending());
+}
+
+int main()
+{
+  assert(wgpu_sync_operations_pending() == 0); // We should start with no asyncified operations running.
+  emscripten_set_timeout(in_between, 0, 0); // We should observe the sync operation.
+
+  WGpuAdapter adapter = navigator_gpu_request_adapter_sync_simple();
+  assert(wgpu_sync_operations_pending() == 0); // All async operations should have resolved now.
+  assert(wgpu_is_adapter(adapter)); // Should have got a valid adapter
+  assert(!wgpu_adapter_is_fallback_adapter(adapter));
+
+  if (ok) // TODO: Grab errors and early quit in emrun harness
+    EM_ASM(window.close());
+}
