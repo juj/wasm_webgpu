0896dcb9c8e14672d2cb51a8ba0bfe2fe2bf462a
Jukka Jylänki <jujjyl@gmail.com>
2024-09-07 22:22:38 +0300

    Simplify test/wgpu_get_num_live_objects.cpp.

--- test/wgpu_get_num_live_objects.cpp
+++ test/wgpu_get_num_live_objects.cpp
@@ -3,13 +3,9 @@
 #include "lib_webgpu.h"
 #include <assert.h>
 
-WGpuAdapter adapter;
-
 void ObtainedWebGpuDevice(WGpuDevice result, void *userData)
 {
   assert(wgpu_get_num_live_objects() == 3); // Adapter, Device and Queue
-  wgpu_object_destroy(adapter);
-  assert(wgpu_get_num_live_objects() == 0);
 
   EM_ASM(window.close());
 }
@@ -17,11 +13,10 @@ void ObtainedWebGpuDevice(WGpuDevice result, void *userData)
 void ObtainedWebGpuAdapter(WGpuAdapter result, void *userData)
 {
   assert(wgpu_get_num_live_objects() == 1); // Adapter
-  adapter = result;
-  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+  wgpu_adapter_request_device_async_simple(result, ObtainedWebGpuDevice);
 }
 
-int main() {
-  assert(wgpu_get_num_live_objects() == 0);
+int main()
+{
   navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
 }
