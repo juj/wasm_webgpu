67a9151fb795ac542c2e2ff43b085df7b4a61c17
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 00:14:03 +0300

    Add test for navigator_gpu_request_adapter_async().

---
+++ test/navigator_gpu_request_adapter_async.cpp
@@ -0,0 +1,25 @@
+// flags: -sEXIT_RUNTIME=0
+// expected-output: navigator.gpu.requestAdapter(options={"forceFallbackAdapter":false,"powerPreference":"low-power"})
+
+#include "lib_webgpu.h"
+#include <assert.h>
+#include <string.h>
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  assert(wgpu_is_adapter(adapter));
+  assert(userData == (void*)42);
+
+  assert(!wgpu_adapter_is_fallback_adapter(adapter));
+
+  EM_ASM(window.close());
+}
+
+int main()
+{
+  WGpuRequestAdapterOptions options = {
+    .powerPreference = WGPU_POWER_PREFERENCE_LOW_POWER,
+    .forceFallbackAdapter = WGPU_FALSE
+  };
+  navigator_gpu_request_adapter_async(&options, ObtainedWebGpuAdapter, (void*)42);
+}
