59607aa75003bd0942fce38fa4952415abb73251
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 13:53:42 +0300

    Add test for wgpu_device_create_pipeline_layout().

---
+++ test/wgpu_device_create_pipeline_layout.cpp
@@ -0,0 +1,22 @@
+// flags: -sEXIT_RUNTIME=0
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+void ObtainedWebGpuDevice(WGpuDevice device, void *userData)
+{
+  WGpuPipelineLayout layout = wgpu_device_create_pipeline_layout(device, 0, 0);
+  assert(layout);
+
+  EM_ASM(window.close());
+}
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+}
+
+int main()
+{
+  navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
+}
