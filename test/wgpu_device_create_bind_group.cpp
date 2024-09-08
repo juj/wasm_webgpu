f5a338ff4cdfa3e08104c762b3e3861f28a9ab05
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 13:55:38 +0300

    Add test for wgpu_device_create_bind_group().

---
+++ test/wgpu_device_create_bind_group.cpp
@@ -0,0 +1,23 @@
+// flags: -sEXIT_RUNTIME=0
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+void ObtainedWebGpuDevice(WGpuDevice device, void *userData)
+{
+  WGpuBindGroupLayout layout = wgpu_device_create_bind_group_layout(device, 0, 0);
+  WGpuBindGroup bg = wgpu_device_create_bind_group(device, layout, 0, 0);
+  assert(bg);
+
+  EM_ASM(window.close());
+}
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+}
+
+int main()
+{
+  navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
+}
