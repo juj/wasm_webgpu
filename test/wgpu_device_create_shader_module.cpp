8355f51e7aadc17a67ffcfe3c5145af2f6df308c
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 13:57:47 +0300

    Add test for wgpu_device_create_shader_module().

---
+++ test/wgpu_device_create_shader_module.cpp
@@ -0,0 +1,25 @@
+// flags: -sEXIT_RUNTIME=0
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+void ObtainedWebGpuDevice(WGpuDevice device, void *userData)
+{
+  WGpuShaderModuleDescriptor desc = {
+    .code = "",
+  };
+  WGpuShaderModule shader = wgpu_device_create_shader_module(device, &desc);
+  assert(shader);
+
+  EM_ASM(window.close());
+}
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+}
+
+int main()
+{
+  navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
+}
