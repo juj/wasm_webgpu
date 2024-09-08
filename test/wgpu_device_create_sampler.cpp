09f2ec5022a1039b9b18936dbe26369c04612fcc
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 13:49:35 +0300

    Add test for wgpu_device_create_sampler().

---
+++ test/wgpu_device_create_sampler.cpp
@@ -0,0 +1,23 @@
+// flags: -sEXIT_RUNTIME=0
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+void ObtainedWebGpuDevice(WGpuDevice device, void *userData)
+{
+  WGpuSamplerDescriptor desc = WGPU_SAMPLER_DESCRIPTOR_DEFAULT_INITIALIZER;
+  WGpuSampler sampler = wgpu_device_create_sampler(device, &desc);
+  assert(sampler);
+
+  EM_ASM(window.close());
+}
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+}
+
+int main()
+{
+  navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
+}
