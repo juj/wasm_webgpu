80496192dddbe723f19469a4e2b62bd7e98746af
Jukka Jylänki <jujjyl@gmail.com>
2024-09-08 13:48:07 +0300

    Add test for wgpu_device_create_texture().

---
+++ test/wgpu_device_create_texture.cpp
@@ -0,0 +1,24 @@
+// flags: -sEXIT_RUNTIME=0
+
+#include "lib_webgpu.h"
+#include <assert.h>
+
+void ObtainedWebGpuDevice(WGpuDevice device, void *userData)
+{
+  WGpuTextureDescriptor desc = WGPU_TEXTURE_DESCRIPTOR_DEFAULT_INITIALIZER;
+  desc.format = WGPU_TEXTURE_FORMAT_RGBA8UNORM; // Specify minimum number of fields that are required for texture creation to succeed.
+  WGpuTexture texture = wgpu_device_create_texture(device, &desc);
+  assert(texture);
+
+  EM_ASM(window.close());
+}
+
+void ObtainedWebGpuAdapter(WGpuAdapter adapter, void *userData)
+{
+  wgpu_adapter_request_device_async_simple(adapter, ObtainedWebGpuDevice);
+}
+
+int main()
+{
+  navigator_gpu_request_adapter_async_simple(ObtainedWebGpuAdapter);
+}
