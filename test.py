07ec3bbf7d35ecc3c830d5629831d3ee9fcd3825
Jukka Jylänki <jujjyl@gmail.com>
2026-03-15 22:54:01 +0200

    Increase browser wait timeout in tests to avoid false positives from too fast timeout.

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ def num_firefox_processes():
   return len(procs.splitlines()[1:])
 
 def wait_firefox_quit():
-  for i in range(10):
+  for i in range(100):
     if num_firefox_processes() == 0:
       return
     time.sleep(0.3)
